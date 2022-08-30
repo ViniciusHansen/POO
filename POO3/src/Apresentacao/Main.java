cb3adcf951cd1ad6a73e830e312a229a3dc3608f
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2022-08-29 21:27:02 -0300

    progresso

--- POO3/src/Apresentacao/Main.java
+++ POO3/src/Apresentacao/Main.java
@@ -1,7 +1,9 @@
 package Apresentacao;
 import java.util.Scanner;
+
+import Dados.Adress;
+import Dados.Animal;
 import Dados.Vet;
-import dados.Veterinario;
 import negocio.SistemaPetShop;
 
 
@@ -35,12 +37,63 @@ public class Main {
 		sistema.cadastrarVeterinario(veterinario);
 	}
 	
-	public void mostrarTodosVeterinarios() {
+	private int mostrarAnimais() {
+		mostrarTodosVeterinarios();
+		System.out.println("Escolha um Veterianáio (pelo indice) para ver os animais que ele cuida:");
+		int i = scan.nextInt();
+		Vet vet = sistema.getVeterinarios()[i];
+		for(int j=0;j<vet.getAnimaisLength();j++) {
+			System.out.printf("Animal %i: %s", j, vet.getAnimais()[j].getNome());
+		}
+		return i;
+	}
+	
+	private void mostrarTodosVeterinarios() {
 		for(int i=0;i<sistema.getQuantVeterinarios();i++) {
 			Vet vet = sistema.getVeterinarios()[i];
 			System.out.println(vet);
 		}
 	}
+	
+	private void cadastrarEnderecoVet() {
+		System.out.println("Veterinarios já cadastrados:");
+		mostrarTodosVeterinarios();
+		System.out.println("Escolha o índice do Vet que deseja cadastrar o endereço:");
+		int i = scan.nextInt();
+		Vet vet = sistema.getVeterinarios()[i];
+		Adress temp = new Adress();
+		System.out.println("rua:");
+		temp.setRua(scan.next());
+		System.out.println("número:");
+		temp.setNumero(scan.next());
+		System.out.println("bairro:");
+		temp.setBairro(scan.next());
+		System.out.println("cidade:");
+		temp.setCidade(scan.next());
+		System.out.println("cep:");
+		temp.setCep(scan.next());
+		vet.setAdress(temp);
+	}
+	
+	private void cadastrarAnimal() {
+		System.out.println("Veterinarios já cadastrados:");
+		mostrarTodosVeterinarios();
+		System.out.println("Escolha o índice do Vet que deseja cadastrar um novo animal:");
+		int i = scan.nextInt();
+		Vet vet = sistema.getVeterinarios()[i];
+		if(vet.getQuantidadeAnimais() < 10) {
+			System.out.println("nome:");
+			Animal temp[] = new Animal[vet.getAnimaisLength()];
+			temp[vet.getQuantidadeAnimais()].setNome(scan.next());
+			System.out.println("espécie:");
+			temp[vet.getQuantidadeAnimais()].setEspecie(scan.next());
+			System.out.println("descrição:");
+			temp[vet.getQuantidadeAnimais()].setDesc(scan.next());
+			vet.setAnimais(temp);
+		}
+	}
+	
+	
 	
 	public static void main(String argv[]) {
 		
@@ -52,22 +105,22 @@ public class Main {
 				  	main.mostrarTodosVeterinarios();
 				  	break;
 			  case 2:
-				  	a.mostrarAnimais();
+				  	main.mostrarAnimais();
 				  	break;
 			  case 3:
 				    main.cadastrarVeterinario();
 				    break;
 			  case 4:
-				    a.cadastrarEnderecoVeterinario();
+				    main.cadastrarEnderecoVet();
 				    break;
 			  case 5:
-				    a.cadastrarAnimal();
+				    main.cadastrarAnimal();
 				    break;
 			  case 6:
-				    a.cadastrarDono();
+				    main.cadastrarDono();
 				    break;
 			  case 7:
-				    a.cadastrarEnderecoDono();
+				    main.cadastrarEnderecoDono();
 				    break;
 			  case 0:
 				    scan.close();
--- POO3/src/negocio/SistemaPetShop.java
+++ POO3/src/negocio/SistemaPetShop.java
@@ -4,7 +4,7 @@ import Dados.Vet;
 public class SistemaPetShop {
 
 	private Vet vets[];
-	private int quantidadeVeterinarios = 0;
+	private int quantidadeVeterinarios;
 	
 	public SistemaPetShop(int tamVet) {
 		vets = new Vet[tamVet];
@@ -26,104 +26,8 @@ public class SistemaPetShop {
 	}
 }
 	/*
-	public Vet getVet() {
-		return vets[quantidadeVeterinarios];
-	}
-	public Vet[] getVets() {
-		return vets;
-	}
-	public void setVets(Vet[] vets) {
-		this.vets = vets;
-	}
-	public int getQuantidadeVeterinarios() {
-		return quantidadeVeterinarios;
-	}
-	public void setQuantidadeVeterinarios(int quantidadeVeterinarios) {
-		this.quantidadeVeterinarios = quantidadeVeterinarios;
-	}
 	
-	public void cadastrarVeterinario() {
-		int qtdVets= getQuantidadeVeterinarios();
-		if (qtdVets < 50) {
-			System.out.println("Nome do Vet: ");
-			vets[qtdVets].setNome(scan.next());
-			System.out.println("Salário do Vet: ");
-			vets[qtdVets].setSalario(Float.valueOf(scan.next()));
-			qtdVets++;
-			setQuantidadeVeterinarios(qtdVets);
-		}
-		else {
-			System.out.println("Lista de vets cheia!");
-		}
-		
-	}
-	
-	public void mostrarVeterinarios() {
-		if (quantidadeVeterinarios > 1) {
-			System.out.println("Lista de Veterinários:");
-			for(int i=0;i<quantidadeVeterinarios;i++) {
-				System.out.printf("Veterinario %i: %s",i,vets[i].getNome());
-			}
-		}
-		else {
-			System.out.println("Nenhum Veterinario cadastrado");
-			return;
-		}
-	}
-	
-	public void cadastrarEnderecoVeterinario() {
-		System.out.println("Veterinarios já cadastrados:");
-		mostrarVeterinarios();
-		System.out.println("Escolha o índice do Vet que deseja cadastrar o endereço:");
-		int i = scan.nextInt();
-		System.out.println("rua:");
-		Adress temp = new Adress();
-		temp.setRua(scan.next());
-		System.out.println("número:");
-		temp.setNumero(scan.next());
-		System.out.println("bairro:");
-		temp.setBairro(scan.next());
-		System.out.println("cidade:");
-		temp.setCidade(scan.next());
-		System.out.println("cep:");
-		temp.setCep(scan.next());
-		vets[i].setAdress(temp);
-	}
-	
-	public void cadastrarAnimal() {
-		System.out.println("Veterinarios já cadastrados:");
-		mostrarVeterinarios();
-		System.out.println("Escolha o índice do Vet que deseja cadastrar um novo animal:");
-		int i = scan.nextInt();	
-		if(vets[i].getQuantidadeAnimais() < 10) {
-			System.out.println("nome:");
-			Animal temp[] = new Animal[vets[i].getAnimaisLength()];
-			temp[vets[i].getQuantidadeAnimais()].setNome(scan.next());
-			System.out.println("espécie:");
-			temp[vets[i].getQuantidadeAnimais()].setEspecie(scan.next());
-			System.out.println("descrição:");
-			temp[vets[i].getQuantidadeAnimais()].setDesc(scan.next());
-			vets[i].setAnimais(temp);
-		}
-	}
-	
-	public int mostrarAnimais() {
-		if (quantidadeVeterinarios > 0) {
-			mostrarVeterinarios();
-		}
-		else {
-			System.out.println("Nenhum Veterinario cadastrado");
-			return -999;
-		}
-		System.out.println("Escolha um Veterianáio(pelo indice) para ver os animais que ele cuida:");
-		int i = scan.nextInt();
-		Animal animais[] = new Animal[vets[i].getAnimaisLength()];
-		for(int j=0;j<vets[i].getAnimaisLength();j++) {
-			animais = vets[i].getAnimais();
-			System.out.printf("Animal %i: %s", j, animais[j].getNome());
-		}
-		return i;
-	}
+
 	
 	public void cadastrarDono() {
 		int a = mostrarAnimais();
