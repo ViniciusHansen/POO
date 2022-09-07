3bb6961b218173b0b04af3dd137a91d8bfc7641c
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2022-09-07 13:56:36 -0300

    finish Ex3

--- Lista1Ex3/src/dados/Imobiliaria.java
+++ Lista1Ex3/src/dados/Imobiliaria.java
@@ -1,5 +1,68 @@
 package dados;
 
+import java.util.Arrays;
+
 public class Imobiliaria {
+	private String nome;
+	private Imovel imoveis[];
+	private int quantImoveis;
+	
+	public int getQuantImoveis() {
+		return quantImoveis;
+	}
+
+	public void setQuantImoveis(int quantImoveis) {
+		this.quantImoveis = quantImoveis;
+	}
+
+	public Imobiliaria(int quantImoveis) {
+		imoveis = new Imovel[quantImoveis];
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Imovel[] getImoveis() {
+		return imoveis;
+	}
+
+	public void setImoveis(Imovel[] imoveis) {
+		this.imoveis = imoveis;
+	}
+
+	Imovel[] filtrarPorArea(float x) {
+		Imovel[] imoveis = this.getImoveis();
+		Imovel[] imoveisMaiores = new Imovel[quantImoveis];
+		int count=0;
+		for(int i=0;i<imoveis.length;i++) {
+			if(imoveis[i].areaImovel()>x) {
+				imoveisMaiores[count]=imoveis[i];
+				count++;
+			}
+		}
+		for(int i=0;i<imoveisMaiores.length;i++) {
+			for(int j=0;j<(imoveisMaiores.length)-1;j++) {
+				Imovel aux = imoveisMaiores[i];
+				imoveisMaiores[i] = imoveisMaiores[i+1];
+				imoveisMaiores[i+1] = aux;
+			}
+		}
+		return imoveisMaiores;
+	}
+	
+	public String toString() {
+		String imoveisStr[] = new String[quantImoveis];
+		for(int i=0;i<quantImoveis;i++) {
+			imoveisStr[i] = imoveis[i].toString();
+		}
+		return "Imobiliaria [nome=" + nome + ", imoveis=" + Arrays.toString(imoveisStr) + "]";
+	}
+	
+	
 	
 }
