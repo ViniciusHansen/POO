13d34b34bc5e22634158349419fb53ce9fc85a6b
Vinícius Hansen <vinicius.carlo.hansen@gmail.com>
2023-01-25 16:53:23 -0300

    deleta códigos triviais & arruma alguns nomes

--- AP7/src/pkg/Quadrados.java
+++
@@ -1,9 +0,0 @@
-package pkg;
-
-public class Quadrados extends Gerador{
-	
-	void gerar(int n) {
-		for(int i=0;i<n;i++)
-			sequencia.add(i*i);
-	}
-}
--- P2/src/dados/FormaGeometrica.java
+++
@@ -1,7 +0,0 @@
-package dados;
-
-public abstract class FormaGeometrica {
-    protected int x,y;
-    public abstract double calculaPerimetro();
-    public abstract double calculaArea();
-}
